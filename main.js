9f1fa35afdabb99fe04b62b769355af4fe693098
Marc Martinez <universal11@Marcs-MacBook-Pro.local>
2014-07-25 13:16:21 -0700

    Updates to stuff

--- main.js
+++ main.js
@@ -28,56 +28,62 @@ var Server = Net.createServer(function(socket){
 
 	//socket.write("Connected to " + socket.localAddress + ":" + socket.localPort + "\r\n");
 	//socket.write("From " + socket.remoteAddress + ":" + socket.remotePort + "\r\n");
+	var request_data = "";
 
 	socket.on("data", function(data){
-		data = data.toString();
 		if(data != "\r\n" && data != ""){
-			var dataAsJsonString = data;
-			try{
-				data = JSON.parse(data);
-			}
-			catch(exception){
-				console.log("Request: " + dataAsJsonString);
-				console.log("Parse error: " + exception);
-				return 0;
-			}
-			console.log("Request: " + UniversalMessengerTransmitter.getActionNameById(data.action) + dataAsJsonString);
-			switch(data.action){
-				case UniversalMessengerTransmitter.ACTIONS.QUIT:
-					socket.end(); 
-					return 0;
-					break;
-				case UniversalMessengerTransmitter.ACTIONS.SEND_MAIL:
-					/*
-					if(!socket.UniversalMessengerTransmitter.is_authenticated){
-						socket.end();
+			data = data.toString();
+			request_data += data;
+			if(request_data.indexOf("\u2404") > -1){
+
+					var dataAsJsonString = request_data;
+					try{
+						data = JSON.parse(request_data);
+					}
+					catch(exception){
+						console.log("Request: " + dataAsJsonString);
+						console.log("Parse error: " + exception);
 						return 0;
 					}
-					*/
-					universalMessengerTransmitter.sendMail(data, socket); 
-					break;
-				case UniversalMessengerTransmitter.ACTIONS.AUTHENTICATE:
-					if(!socket.UniversalMessengerTransmitter.is_authenticated){
-						universalMessengerTransmitter.authenticate(data, socket);
+					console.log("Request: " + UniversalMessengerTransmitter.getActionNameById(data.action) + dataAsJsonString);
+					switch(data.action){
+						case UniversalMessengerTransmitter.ACTIONS.QUIT:
+							socket.end(); 
+							return 0;
+							break;
+						case UniversalMessengerTransmitter.ACTIONS.SEND_MAIL:
+							/*
+							if(!socket.UniversalMessengerTransmitter.is_authenticated){
+								socket.end();
+								return 0;
+							}
+							*/
+							universalMessengerTransmitter.sendMail(data, socket); 
+							break;
+						case UniversalMessengerTransmitter.ACTIONS.AUTHENTICATE:
+							if(!socket.UniversalMessengerTransmitter.is_authenticated){
+								universalMessengerTransmitter.authenticate(data, socket);
+							}
+							break;
+						case UniversalMessengerTransmitter.ACTIONS.GET_IP_ADDRESS_DATA:
+							if(!socket.UniversalMessengerTransmitter.is_authenticated){
+								socket.end();
+								return 0;
+							}
+							universalMessengerTransmitter.getIpAddressData(socket);
+							break;
+						case UniversalMessengerTransmitter.ACTIONS.PROCESS_HTML_TO_IMAGE:
+							if(!socket.UniversalMessengerTransmitter.is_authenticated){
+								socket.end();
+								return 0;
+							}
+							//universalMessengerTransmitter.processHTMLtoImage(data, dbConnection, socket); 
+							break;
+						default:
+							break;
 					}
-					break;
-				case UniversalMessengerTransmitter.ACTIONS.GET_IP_ADDRESS_DATA:
-					if(!socket.UniversalMessengerTransmitter.is_authenticated){
-						socket.end();
-						return 0;
-					}
-					universalMessengerTransmitter.getIpAddressData(socket);
-					break;
-				case UniversalMessengerTransmitter.ACTIONS.PROCESS_HTML_TO_IMAGE:
-					if(!socket.UniversalMessengerTransmitter.is_authenticated){
-						socket.end();
-						return 0;
-					}
-					//universalMessengerTransmitter.processHTMLtoImage(data, dbConnection, socket); 
-					break;
-				default:
-					break;
 			}
+
 		}
 
 	});
